8eb7f466f6da3db170423dccd853e8c3e4e2343f
angyts <angyts@theopendao.com>
2022-04-15 11:55:34 +0800

    add test block

---
+++ blocks/web3/lang/en/block_web3.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Add a Web3 Block';
+$string['web3'] = 'Web3 Block';
+$string['web3:addinstance'] = 'Add a new simple HTML block';
+$string['web3:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';
